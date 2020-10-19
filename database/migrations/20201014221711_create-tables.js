ebe721d1be2cff1389ddcb409c3bd35573433802
Janneth Williams <47696793+Jwilliams85@users.noreply.github.com>
2020-10-19 15:29:49 -0400

    added tables

--- database/migrations/20201014221711_create-tables.js
+++ database/migrations/20201014221711_create-tables.js
@@ -1,16 +1,24 @@
 exports.up = function(knex) {
-    return knex.schema.createTable('users', users => {
-      users.increments();
-  
-      users
-        .string('username', 255)
-        .notNullable()
-        .unique();
-      users.string('password', 255).notNullable();
-    });
-  };
-  
-  exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('users');
-  };
-  
+  return (
+    knex.schema
+      .createTable("users", tbl => {
+        tbl.increments();
+        tbl.string("fullname", 128).notNullable();
+        tbl.string("username", 128).notNullable().unique();
+        tbl.string("password", 128).notNullable();
+        tbl.string("phonenumber", 11);
+      })
+      .createTable("plants", tbl => {
+        tbl.increments();
+        tbl.string("plant_name", 256).notNullable();
+        tbl.string("plant_species", 256).notNullable();
+        tbl.string("water_schedule", 512).notNullable();
+        
+      })
+  );
+};
+
+exports.down = function(knex) {
+  // reverse order of creation
+  return knex.schema.dropTableIfExists("plants").dropTableIfExists("users");
+};
